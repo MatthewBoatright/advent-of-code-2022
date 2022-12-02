8028ade6f807cb9b362d2260e6ce5204e341e32e
MatthewBoatright <matthew.c.boatright@gmail.com>
2022-12-02 18:23:12 -0500

    Day 1. Problem 2 of 2.

--- day-1/calorie_counter.rb
+++ day-1/calorie_counter.rb
@@ -1,18 +1,21 @@
-def count_max_calories(input)
+def count_max_calories(input, limit = 3)
   puts "Opening input file: #{input}"
 
-  max_calories = 0
+  max_calories = Array.new(limit, 0)
   cur_calories = 0
   File.foreach(input) do |line|
     if line == "\n"
-      max_calories = [max_calories, cur_calories].max
+      max_calories[0] = cur_calories if cur_calories > max_calories.first
+      max_calories.sort!
+
       cur_calories = 0
     else
       cur_calories += line.to_i
     end
   end
 
-  [max_calories, cur_calories].max
+  max_calories[0] = cur_calories if cur_calories > max_calories.first
+  max_calories.sum
 end
 
 if ARGV.length != 1
