cdf1c44043b6a02cb35750a508261920f41e77fa
MatthewBoatright <matthew.c.boatright@gmail.com>
2022-12-15 20:28:32 -0500

    Day 7. Part 1 of 2.

---
+++ day-7/disk_saver.rb
@@ -0,0 +1,34 @@
+def process_input(input)
+  res = Hash.new(0)
+  cwd = []
+
+  File.foreach(input) do |line|
+    line = line.split(' ')
+    case line
+    in ['$', 'cd', '..']
+      cwd.pop
+    in ['$', 'cd' '/']
+      cwd = ['/']
+    in ['$', 'cd', dir]
+      cwd << dir
+    in ['$', 'ls']
+    in ['dir', dir]
+    in [size, filename]
+      cwd.length.times do |i|
+        res[cwd[0..i]] += size.to_i
+      end
+    end
+  end
+
+  res
+end
+
+if ARGV.length < 1
+  puts "Usage: ruby disk_saver.rb <input_file>"
+  exit 1
+end
+
+input = ARGV[0]
+dir_sizes = process_input(input)
+
+puts dir_sizes.select{ |k,v| v <= 100000 }.values.sum
